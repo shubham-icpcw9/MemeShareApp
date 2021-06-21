405270318d35c42f429cfa15d0d989901862aad8
Shubham Kumar Patwarika <shubhampatwarika@gmail.com>
2021-06-22 01:55:11 +0530

    Fixed some bugs.

--- app/src/main/java/com/example/memeshareapp/MainActivity.kt
+++ app/src/main/java/com/example/memeshareapp/MainActivity.kt
@@ -2,9 +2,7 @@ package com.example.memeshareapp
 
 import android.content.Intent
 import android.graphics.drawable.Drawable
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -18,7 +16,8 @@ import com.bumptech.glide.request.target.Target
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private var currImgUrl : String? = null
+    private lateinit var currImgUrl : String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -35,35 +34,36 @@ class MainActivity : AppCompatActivity() {
         // Instantiate the RequestQueue.
         //val queue = Volley.newRequestQueue(this)
         progressBar.visibility = View.VISIBLE
-        val url = " https://meme-api.herokuapp.com/gimme"
+        val url = "https://meme-api.herokuapp.com/gimme"
 
-        // Request a string response from the provided URL.
+        // Request a JSON response from the provided URL.
         val jsonObjectRequest = JsonObjectRequest(
             Request.Method.GET, url, null,
             { response ->
-                currImgUrl = response.getString("url")
-                Glide.with(this).load(currImgUrl).listener(object : RequestListener<Drawable> {
-                    override fun onLoadFailed(
-                        e: GlideException?,
-                        model: Any?,
-                        target: Target<Drawable>?,
-                        isFirstResource: Boolean
-                    ): Boolean {
-                        return false
-                    }
+                 currImgUrl = response.getString("url")
+                 Glide.with(this).load(currImgUrl).listener(object : RequestListener<Drawable> {
+                     override fun onLoadFailed(
+                         e: GlideException?,
+                         model: Any?,
+                         target: Target<Drawable>?,
+                         isFirstResource: Boolean
+                     ): Boolean {
+                         progressBar.visibility = View.GONE
+                         return false
+                     }
 
-                    override fun onResourceReady(
-                        resource: Drawable?,
-                        model: Any?,
-                        target: Target<Drawable>?,
-                        dataSource: DataSource?,
-                        isFirstResource: Boolean
-                    ): Boolean {
-                        progressBar.visibility = View.GONE
-                        return false
-                    }
-                }).into(memeIv)
-            },
+                     override fun onResourceReady(
+                         resource: Drawable?,
+                         model: Any?,
+                         target: Target<Drawable>?,
+                         dataSource: DataSource?,
+                         isFirstResource: Boolean
+                     ): Boolean {
+                         progressBar.visibility = View.GONE
+                         return false
+                     }
+                 }).into(memeIv)
+             },
             { Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show() })
 
         // Add the request to the RequestQueue.
@@ -74,7 +74,8 @@ class MainActivity : AppCompatActivity() {
     fun shareMeme(view: View) {
         val intent = Intent(Intent.ACTION_SEND)
         intent.type = "text/plain"
-        intent.putExtra(Intent.EXTRA_TEXT, "Hey checkout this cool meme from redit $currImgUrl")
+        intent.putExtra(Intent.EXTRA_TEXT, "Hey checkout this cool meme from reddit $currImgUrl")
+        //Create a Chooser Dialog
         val chooser = Intent.createChooser(intent, "Share this meme using")
         startActivity(chooser)
     }
